42efd585ed10a57eaca614d843d1f9af852e4f45
James <james.rose1103@protonmail.com>
2024-09-19 15:21:21 -0400

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,57 @@
+packages = ('cask-server', maui-core, maui-shell)
+
+for package in packages:
+    cd $package
+    makepkg -f
+    cd ..
+
+sudo pacman -S --noconfirm bluedevil \
+         fontconfig \
+         karchive \
+         kconfig \
+         kcoreaddons \
+         kcrash \
+         kdbusaddons \
+         kdeclarative \
+         kded \
+         kdesu \
+         # kdoctools \
+         kglobalaccel \
+         ki18n \
+         kiconthemes \
+         kidletime \
+         kinit \
+         kio \
+         kitemmodels \
+         knotifications \
+         knotifyconfig \
+         kpackage \
+         kpeople \
+         krunner \
+         kservice \
+         ktexteditor \
+         kunitconversion \
+         kwallet \
+         kwayland \
+         mauikit-calendar \
+         mauikit-filebrowsing \
+         mauikit \
+         mauiman \
+         phonon-qt6 \
+         plasma-nm \
+         plasma-pa \
+         polkit-qt6 \
+         prison \
+         qt6-base \
+         qt6-declarative \
+         qt6-quickcontrols2 \
+         qt6-svg \
+         qt6-wayland \
+         solid
+
+cd maui-core
+makepkg -si
+cd ../cask-server
+makepkg -si
+cd ../maui-shell
+makepkg -si
